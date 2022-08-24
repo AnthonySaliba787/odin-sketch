3d9f6ae25b2ef568e58ef4f892dde882ac0e616e
Anthony <anthonysaliba93@gmail.com>
2022-08-24 11:09:45 +0300

    Add button and style it

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const container = document.createElement("div");
 container.setAttribute("id", "grid-container");
 document.body.appendChild(container);
 
+let squares = 0;
+
 for (i = 0; i < 256; i++) {
   let tempdiv = document.createElement("div");
   tempdiv.classList.toggle("grid-item");
