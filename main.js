b47c05b0bf9a6b5be47376b075c866349b295339
Anthony <anthonysaliba93@gmail.com>
2022-08-24 11:00:03 +0300

    Create and style the grid system

---
+++ main.js
@@ -0,0 +1,12 @@
+const container = document.createElement("div");
+container.setAttribute("id", "grid-container");
+document.body.appendChild(container);
+
+for (i = 0; i < 256; i++) {
+  let tempdiv = document.createElement("div");
+  tempdiv.classList.toggle("grid-item");
+  container.appendChild(tempdiv);
+}
+
+const divs = document.querySelectorAll("div");
+divs.forEach((div) => {});
