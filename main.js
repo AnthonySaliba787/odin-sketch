3e2fe2b602f1f531a78b6e09224fb30a2403baec
Anthony <anthonysaliba93@gmail.com>
2022-08-24 11:38:02 +0300

    Add the code to changing squares while keeping aspect ratio

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const container = document.createElement("div");
 container.setAttribute("id", "grid-container");
 document.body.appendChild(container);
 
-let squares = 0;
+container.style.gridTemplateColumns = "repeat(16, 1fr)";
+container.style.gridTemplateRows = "repeat(16, 1fr)";
 
 for (i = 0; i < 256; i++) {
   let tempdiv = document.createElement("div");
@@ -10,5 +11,31 @@ for (i = 0; i < 256; i++) {
   container.appendChild(tempdiv);
 }
 
-const divs = document.querySelectorAll("div");
-divs.forEach((div) => {});
+const button = document.querySelector(".add-squares");
+let squares = 0;
+
+button.addEventListener("click", () => {
+  // We take input and limit it to 100 squares at most
+  squares = parseInt(prompt("Please enter the number of squares:"));
+  while (squares > 100) {
+    squares = parseInt(
+      prompt("Please enter a number smaller or equal to 100:")
+    );
+  }
+
+  // We clear the grid
+  let divSquares = document.getElementsByClassName("grid-item");
+  while (divSquares.length > 0) {
+    container.removeChild(divSquares[0]);
+  }
+
+  container.style.gridTemplateColumns = "repeat(" + squares + ", 1fr)";
+  container.style.gridTemplateRows = "repeat(" + squares + ", 1fr)";
+  let calculated_squares = squares * squares;
+  // We populate the new grid with the specified number
+  for (i = 0; i < calculated_squares; i++) {
+    let tempdiv = document.createElement("div");
+    tempdiv.classList.toggle("grid-item");
+    container.appendChild(tempdiv);
+  }
+});
